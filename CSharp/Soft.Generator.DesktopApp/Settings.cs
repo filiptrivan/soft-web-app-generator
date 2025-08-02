815d95c2020e0343e47335ab8e0f6fc9f13624c4
filiptrivan <120225885+filiptrivan@users.noreply.github.com>
2025-08-02 02:04:58 +0200

    Update Settings.cs

--- CSharp/Soft.Generator.DesktopApp/Settings.cs
+++ CSharp/Soft.Generator.DesktopApp/Settings.cs
@@ -12,10 +12,6 @@ namespace Soft.Generator.DesktopApp
 
         public static string EntitiesNamespaceEnding = "Entities";
         public static string DTONamespaceEnding = "DTO";
-        public static string DownloadPath = @"C:\Users\user\Downloads";
-        public static string ClientApplicationBasePath = @""; // Dodaj dodatno dugme, generisi UI strukturu
-        public static string GeneralStylesFrontendPath = @"C:\Users\user\Documents\Projects\PlayertyLoyals\playerty-loyals\Angular\src\assets\primeng\styles";
-        public static string GeneralCoreFrontendPath = @"C:\Users\user\Documents\Projects\PlayertyLoyals\playerty-loyals\Angular\src\app\core";
         public static string PrimaryColor = @"#cccccc";
         public static string ProjectsPath = @"C:\Users\user\Documents\Projects"; // Add this into app table.
     }
